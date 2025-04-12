3cd7c693fdf861d69ae33732b87ddbd7578210b7
Jeremy Cavallo <jcbball55@gmail.com>
2025-04-12 11:54:15 -0400

    Fixed diagnostic key mapping

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,5 +1,6 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pv', vim.cmd.Ex, {})
+vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
